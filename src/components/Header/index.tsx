d1e1f67e781152dfcddf8bcbe3973b3a0052658a
Agustin <agustinescalanterojo@gmail.com>
2023-07-26 12:28:07 +0200

    feat: implement models

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FC } from "react";
+
+const Header: FC = () => {
+    return (
+        <header className="header">
+            <div className="content">
+                <h2 className="title">Rent a car</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/views/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {FC} from "react"
+import Header from "../../components/Header"
+
+const Dashboard: FC = () => {
+    return (
+        <div>
+           <Header/>
+        </div>
+    )
+}
+
+export default Dashboard
